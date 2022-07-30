bf5a594aa52597993952c4ed2a72d9cdeb0c3ce9
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-30 11:08:26 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,1 @@
+<h1 class="bg-zinc-800 text-zinc-50 text-4xl font-bold h-screen grid place-content-center">Hello world</h1>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app.html", "./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
