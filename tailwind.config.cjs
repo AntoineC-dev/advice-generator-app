6fececdc2bb3e4b6a81738533850e61b57d00ed8
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-30 23:07:18 -0400

    Create theme variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/app.html", "./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      cyan: "hsl(193, 38%, 86%)",
+      green: "hsl(150, 100%, 66%)",
+      blue: {
+        light: "hsl(217, 19%, 38%)",
+        DEFAULT: "hsl(217, 19%, 24%)",
+        dark: "hsl(218, 23%, 16%)",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
