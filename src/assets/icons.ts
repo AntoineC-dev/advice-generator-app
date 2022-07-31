0ae945f9e9c56af0b0de7b1a9b9fd6fecb2f7874
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-31 07:14:02 -0400

    Create assets folder

---
+++ src/assets/icons.ts
@@ -0,0 +1,4 @@
+export { default as mobileDivider } from "./pattern-divider-mobile.svg";
+export { default as desktopDivider } from "./pattern-divider-desktop.svg";
+export { default as diceSvg } from "./icon-dice.svg";
+export { default as spinnerSvg } from "./spinner.svg";
